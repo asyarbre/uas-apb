0af1fe8e43a347d122cb74f0541f66032ed54fa8
Asya'ri <asyari818@gmail.com>
2023-05-28 20:27:41 +0700

    feature add to calendar

--- app/src/main/java/com/example/uasapb/DetailScheduleActivity.kt
+++ app/src/main/java/com/example/uasapb/DetailScheduleActivity.kt
@@ -1,10 +1,14 @@
 package com.example.uasapb
 
+import android.content.Intent
 import android.os.Bundle
+import android.provider.CalendarContract
+import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.example.uasapb.fragment.ScheduleFragment
+import java.util.*
 
 class DetailScheduleActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -27,6 +31,44 @@ class DetailScheduleActivity : AppCompatActivity() {
         val scheduleBackBtn = findViewById<ImageView>(R.id.schedule_back)
         scheduleBackBtn.setOnClickListener {
             finish()
+        }
+
+        //split date race to array like Jun 5-7 to [Jun, 5, 7]
+        val date = Schedule.date
+        val dateSplit = date.split(Regex(" |-"))
+        val month = dateSplit[0]
+        val startDate = dateSplit[1].toInt()
+        val endDate = dateSplit[2].toInt()
+
+        // convert month to number, like Jan to 0, Feb to 1, etc
+        fun convertMonth(monthName: String): Int {
+            val listMonth = listOf("Jan", "Feb", "Mar", "Apr", "May", "Jun",
+                "Jul", "Aug", "Sep", "Oct", "Nov", "Dec")
+            return listMonth.indexOf(monthName)
+        }
+
+        val monthNumber = convertMonth(month)
+
+        val startMillis: Long = Calendar.getInstance().run {
+            set(2023, monthNumber, startDate, 7, 30)
+            timeInMillis
+        }
+
+        val endMillis: Long = Calendar.getInstance().run {
+            set(2023, monthNumber, endDate, 7, 30)
+            timeInMillis
+        }
+
+        //insert date race to calendar app on device
+        val btnAdd = findViewById<Button>(R.id.btn_add_calendar)
+        btnAdd.setOnClickListener {
+            val intent = Intent(Intent.ACTION_INSERT)
+                .setData(CalendarContract.Events.CONTENT_URI)
+                .putExtra(CalendarContract.Events.TITLE, Schedule.nameRace)
+                .putExtra(CalendarContract.Events.EVENT_LOCATION, Schedule.nameCircuit)
+                .putExtra(CalendarContract.EXTRA_EVENT_BEGIN_TIME, startMillis)
+                .putExtra(CalendarContract.EXTRA_EVENT_END_TIME, endMillis)
+            startActivity(intent)
         }
 
     }
